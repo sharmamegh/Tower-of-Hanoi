7629fdc0c415b27fc3df648ebdae04f2c7770b4c
Megh ✨ <71490844+sharmamegh@users.noreply.github.com>
2023-08-26 10:47:12 +0530

    First upload of source code

---
+++ ToH.cpp
@@ -0,0 +1,65 @@
+#include "iostream"
+
+using namespace std;
+
+// utilizing concept of stack data structure to construct the required tower.
+class Tower {
+	friend void ToH(int, Tower, Tower, Tower);
+	private:
+		int * arr;
+		int top, size;
+	public:
+		Tower() {}
+		Tower(int size) {
+			this->size = size; // explicit use of instance variable
+			arr = new int[size]; // created array of pointers to handle blocks
+			top = 0;
+			arr[1] = 0;
+		}
+		
+		void buildSource() {
+			int tempSize = size;
+			for (; tempSize > 0; tempSize--) {
+				arr[++top] = top;
+			}
+			cout << "Source tower built:" << endl;
+			display();
+		}
+		
+		void display() {
+			if (arr[1] == 0) {
+				cout <<  "Tower currently empty." << endl;
+			} else {
+				for (int i = top; i > 0; i--)
+					cout << arr[i] << " ";
+				cout << endl;
+			}
+		}
+};
+
+int  main() {
+	int capacity; // var containing capacity of the tower, i.e. A tower of capacity n will have n blocks to be moved.A Working Tower of Hanoi Program
+	cout << "" << endl
+		<< "Enter Tower Capacity: "; // used concept of cascading i/o
+	cin >> capacity;
+	
+	// Source Tower
+	Tower S(capacity); // constructor of Tower class implicitly called.
+	S.buildSource(); // building the Source tower.
+	//Helper/Auxiliary Tower
+	Tower H(capacity);
+	// Destination Tower
+	Tower D(capacity);
+	
+	ToH(capacity, S, H, D);
+	
+	return 0;
+}
+
+void ToH(int cap, Tower S, Tower H, Tower D) {
+	
+	
+	cout<< "Source status: "; S.display();
+	cout<< "Helper status: "; H.display();
+	cout<< "Destination status: "; D.display();
+}
